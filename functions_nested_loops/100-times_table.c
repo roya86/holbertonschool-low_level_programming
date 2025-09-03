76e5fcc78f089e77d9d8233286eaec7f77f12f41
roya86 <royaabduallah@gmail.com>
2025-09-03 13:30:01 +0000

    Add function 100-times_table.c

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,49 +1,27 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
+ * @n: the number of the times table
  */
 void print_times_table(int n)
 {
-	int i, j, result;
+	int i, j, prod;
 
-	if (n >= 0 && n <= 15)
-	{
+	if (n < 0 || n > 15)
+	return;
+
 	for (i = 0; i <= n; i++)
 	{
 	for (j = 0; j <= n; j++)
 	{
-	result = i * j;
-
+	prod = i * j;
 	if (j == 0)
-	_putchar('0');
+	printf("%d", prod);
 	else
-	{
-	_putchar(',');
-	_putchar(' ');
-
-	if (result < 10)
-	{
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(result + '0');
+	printf("%4d", prod);
 	}
-	else if (result < 100)
-	{
-	_putchar(' ');
-	_putchar((result / 10) + '0');
-	_putchar((result % 10) + '0');
-	}
-	else
-	{
-	_putchar((result / 100) + '0');
-	_putchar(((result / 10) % 10) + '0');
-	_putchar((result % 10) + '0');
-	}
-	}
-	}
-	_putchar('\n');
-	}
+	printf("\n");
 	}
 }
