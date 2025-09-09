ad018e9585983cc5f0c4425794aa222efe8e5dfc
roya86 <royaabduallah@gmail.com>
2025-09-09 11:54:52 +0000

    Add 1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
 /**
- * _mempcy - copies memory area
- * @dest: destination memory area
- * @src: source memory area
+ * _memcpy - copies memory area
+ * @dest: pointer to the destination memory area
+ * @src: pointer to the source memory area
  * @n: number of bytes to copy
- *
- * Description: This function copies n bytes from the memory area
- * pointed to by src to the memory area pointed to by dest.
  *
  * Return: pointer to dest
  */
@@ -19,5 +16,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	{
 	dest[i] = src[i];
 	}
+
 	return (dest);
 }
