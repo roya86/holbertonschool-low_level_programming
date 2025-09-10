bad3796ceaf8256bf5ada2c64cbf23075255b8ca
roya86 <royaabduallah@gmail.com>
2025-09-10 16:21:29 +0000

    Add 7-print_chessboard.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -2,17 +2,18 @@
 
 /**
  * print_chessboard - prints the chessboard
- * a: array with 8 rows and 8 column
+ * @a: array of characters (2D)
  */
-void print_the_chessboard(char (*a)[8])
+void print_chessboard(char (*a)[8])
 {
 	int i, j;
+
 	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
-		_putchar('\n');
+	for (j = 0; j < 8; j++)
+	{
+	_putchar(a[i][j]);
+	}
+	_putchar('\n');
 	}
 }
