16a9bf693c663f2a0466a01364367dc2b3226077
roya86 <royaabduallah@gmail.com>
2025-09-10 11:39:22 +0000

    Fix 5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,18 +11,20 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i;
+
 	if (*needle == '\0')
-		return (haystack);
+	return (haystack);
+
 	while (*haystack)
 	{
-		i = 0;
-		while (needle[i] && haystack[i] == needle[i])
-		{
-			i++;
-		}
-		if (needle[i] == '\0')
-			return (haystack);
-		haystack++;
+	i = 0;
+	while (needle[i] && haystack[i] == needle[i])
+	{
+	i++;
+	}
+	if (needle[i] == '\0')
+	return (haystack);
+	haystack++;
 	}
 	return (0);
 }
