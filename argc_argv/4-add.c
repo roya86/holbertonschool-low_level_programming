b47956665c460ffbb776206ae5289d452388e3eb
roya86 <royaabduallah@gmail.com>
2025-09-13 10:38:34 +0000

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * is_number - checks if a string is a positive number
+ * @s: string to check
+ *
+ * Return: 1 if number, 0 otherwise
+ */
+int is_number(char *s)
+{
+	int i = 0;
+
+	if (s[0] == '\0')
+		return (0);
+
+	while (s[i])
+	{
+		if (!isdigit(s[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * main - adds positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if success, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!is_number(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
