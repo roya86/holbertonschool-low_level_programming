6dbd30527c281cec838591713725a1a150cae1c5
roya86 <royaabduallah@gmail.com>
2025-09-14 07:03:39 +0000

    Add create_array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include<stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars and initializes it with a specific char
+ * @size: The size of the array
+ * @c: The char to initialize the array with
+ *
+ * Return: Pointer to the array, or NULL if it fails or if size = 0
+ */
+char *create_array(unsigned int size, char c) 
+{
+	char *array;
+	unsigned int i;
+	if (size ==0)
+		return (NULL);
+	array = malloc(size * sizeof(char));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		array[i] = c;
+	return (array);
+}
+
---
+++ malloc_free/README.md
@@ -0,0 +1,1 @@
+This project is about practicing dynamic memory allocation in C using malloc and free.
