9dbd7e133bf4489d25fa4ec962f141c221ad1131
roya86 <royaabduallah@gmail.com>
2025-09-14 07:48:05 +0000

    Fix 0-create_array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -8,11 +8,11 @@
  *
  * Return: Pointer to the array, or NULL if it fails or if size = 0
  */
-char *create_array(unsigned int size, char c) 
+char *create_array(unsigned int size, char c)
 {
 	char *array;
 	unsigned int i;
-	if (size ==0)
+	if (size == 0)
 		return (NULL);
 	array = malloc(size * sizeof(char));
 	if (array == NULL)
