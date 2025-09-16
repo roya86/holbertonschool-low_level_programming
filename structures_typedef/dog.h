3d0f46242a68ff4b334a0ed32fe43fb7e30d3233
roya86 <royaabduallah@gmail.com>
2025-09-16 09:53:50 +0000

    Add dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure that defines a dog
+ * @name: name of the dog (string)
+ * @age: age od the dog (float)
+ * @owner: owner of the dog (string)
+ */
+struct dog
+{
+	char *name*
+	float age;
+	char *owner;
+};
+#endif
