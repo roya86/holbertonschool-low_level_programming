19ad1bd34e2c6b27a412b3fd60be5ff477dfe9a7
roya86 <royaabduallah@gmail.com>
2025-09-26 14:12:56 +0300

    Update 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -13,7 +13,8 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (argv[2][1] != '\0' || (opf = get_op_func(argv[2])) == NULL)
+	opf = get_op_func(argv[2]);
+	if (argv[2][1] != '\0' || opf == NULL)
 	{
 		printf("Error\n");
 		exit(99);
