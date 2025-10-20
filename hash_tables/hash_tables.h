25810000730e798a7626cd29a9871d69aef11593
roya86 <royaabduallah@gmail.com>
2025-10-20 06:46:35 +0000

    update hash_tables.h

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -20,8 +20,8 @@ typedef struct hash_node_s
  *
  * @size: The size of the array
  * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list,
- * because we want our HashTable to use a chaining collision handling
+ *
+ * Description: structure that represent a hash table
  */
 typedef struct hash_table_c
 {
